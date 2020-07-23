ab2222b87fb221b08d5860545c6d995c872e758c
marmarwin0 <marmarw161@gmail.com>
2020-07-23 09:20:14 +0630

    Assignment1

---
+++ actionpage.php
@@ -0,0 +1,15 @@
+<?php
+$name=$_GET["name"];
+$email=$_GET["email"];
+$phone=$_GET["phone"];
+$township=$_GET["township"];
+$grade=$_GET["grade"];
+$subject=$_GET["subject"];
+echo "Welcome ".$name."<br>";
+echo "Your Email Address is ".$email."<br>";
+echo "Your Phno is ".$phone."<br>";
+echo "Your Township is ".$township."<br>";
+echo "Your Grade is ".$grade."<br>";
+echo "Your subject is ".$subject."<br>";
+
+?>
